6faa9e38a4eebc94e23dfd2ad61e9013c98e3276
lucero <moreta.concepcion@gmail.com>
2022-10-12 16:24:01 -0400

    initial files

---
+++ books_store/Controllers/BooksController.cs
@@ -0,0 +1,40 @@
+﻿using booksContext.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace books_store.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BooksContext _dbContext;
+        public BooksController(BooksContext booksContext)
+        {
+            _dbContext = booksContext;
+        }
+
+
+        //GET: api/Movies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Books>>> GetBooks()
+        {
+            if (_dbContext.Books == null)
+            {
+                return NotFound();
+            }
+            return await _dbContext.Books.ToListAsync();
+        }
+        [HttpPost]
+        public async Task<ActionResult<Books>>PostBook(Books book)
+        {
+            _dbContext.Books.Add(book);
+            await _dbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetBooks), new { id = book.Id }, book);
+        }
+
+    }
+}
---
+++ books_store/Models/BooksContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace booksContext.Models
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext(DbContextOptions<BooksContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Books> Books { get; set; } = null!;
+
+    }
+}
